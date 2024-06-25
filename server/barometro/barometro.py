aef31585218948465ae14236dd92134e49aacf60
Santiago78op <2993696170101@ingenieria.usac.edu.gt>
2024-06-25 15:55:21 -0600

    Cambio a funciones barometro cambio de valores arreglo

--- server/barometro/barometro.py
+++ server/barometro/barometro.py
@@ -5,7 +5,7 @@ import os
 lib = ctypes.CDLL(os.path.abspath("barometro.so"))
 
 # Crear un arreglo en Python
-py_array = [1, 2, 3, 4, 5]
+py_array = [6,2,3,1]
 
 # Convertir el arreglo de Python a un arreglo de C
 c_array = (ctypes.c_int * len(py_array))(*py_array)
