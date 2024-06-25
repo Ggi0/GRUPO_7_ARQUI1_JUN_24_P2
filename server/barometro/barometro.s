69c9bd8060196231e18c20d12bbeb7cf9348f155
Santiago78op <2993696170101@ingenieria.usac.edu.gt>
2024-06-25 16:30:17 -0600

    Cambio a funciones barometro cambio

--- server/barometro/barometro.s
+++ server/barometro/barometro.s
@@ -10,8 +10,8 @@ suma_loop:
     cbz x1, media
 
     // Sumar el valor actual del arreglo a x2
-    ldr s3, [x0], #4
-    fadd d2, d2, s3
+    ldr w3, [x0], #4
+    add d2, d2, w3 uxtw
 
     // Decrementar x1 y continuar con el siguiente elemento
     sub x1, x1, #1
@@ -22,39 +22,8 @@ suma_loop:
 media:
     // Dividimos el valor total por la cantidad de elementos
     mov  x1, x3
-    mov  x4, x3
-    fdiv d0, d2, x3
-
-total_loop:
-    // Si x1 (la longitud del arreglo) es 0, hemos terminado
-    cbz x1, divide
-
-    // Sumar el valor actual del arreglo a x2
-    ldr s4, [x0], #4
-    // resta los valores en absoluto
-    fsub d4, d0, s4
-    // Obtiene el valor absoluto
-    fabs d4, d4
-    // hace la operacion de potencia
-    fmul d5, d4, d4
-
-    // suma los valores de potencia
-    fadd d6, d6, d5 
-    // Decrementar x1 y continuar con el siguiente elemento
-    sub x1, x1, #1
-    // Incrementa el contador de valores
-    add x5, x5, #1
-    b total_loop
-
-divide:
-    // divimos el valor
-    ldr s5, [x5]
-    fdiv d7, d6, s5
-
-    // generamos raiz
-    //Calcular la raiz cuadrada del registro d0
-    fsqrt d1, d7
+    div d0, d2, x3
 
     // Mover el resultado (la suma) a x0 para devolverlo
-    mov x0, d1
+    mov x0, d0
     ret
