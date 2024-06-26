2b1755b49e74cb49b549f85462edcb429c5bc9ec
Santiago78op <2993696170101@ingenieria.usac.edu.gt>
2024-06-26 09:26:53 -0600

    Cambio a funciones barometro cambio 7

--- server/barometro/barometro.py
+++ server/barometro/barometro.py
@@ -5,15 +5,16 @@ import os
 lib = ctypes.CDLL(os.path.abspath("barometro.so"))
 
 # Crear un arreglo en Python
-py_array = [6,2,3,2]
+py_array = [6.0, 2.01, 3.23, 2.1]
 
 # Convertir el arreglo de Python a un arreglo de C
-c_array = (ctypes.c_int * len(py_array))(*py_array)
+c_array = (ctypes.c_float * len(py_array))(*py_array)
 
 def main():
     
     # Declarar los tipos de retorno y argumentos de las funciones ensamblador
     lib.sum_array.restype = ctypes.c_float
+    lib.sum_array.argtypes = [ctypes.POINTER(ctypes.c_float), ctypes.c_int]
     result = lib.sum_array(c_array, len(py_array))
     print(f"Resultado de la suma: {result}")
 
--- server/barometro/barometro.s
+++ server/barometro/barometro.s
@@ -1,31 +1,28 @@
+.section .data
+fmt_float_add: .asciz "%.2f\n"
+
 .global sum_array
 sum_array:
     // Los primeros dos argumentos (el arreglo y su longitud) se pasan en los registros x0 y x1
-    // Inicializar d2 (que usaremos para la suma) a 0
-    mov x2, #0
+    // Inicializar d0 (que usaremos para la suma) a 0
+    fmov d0, #0
 
 // Crear un bucle que recorra el arreglo
 suma_loop:
     // Si x1 (la longitud del arreglo) es 0, hemos terminado
-    cbz x1, media
+    cbz x1, fin
 
-    // Sumar el valor actual del arreglo a x2
-    ldr w3, [x0], #4
-    add x2, x2, w3, uxtw
+    // Sumar el valor actual del arreglo a d0
+    ldr s2, [x0], #4
+    fadd d0, d0, s2
 
     // Decrementar x1 y continuar con el siguiente elemento
     sub x1, x1, #1
-    // Incrementa el contador de valores
-    add x3, x3, #1
     b suma_loop
 
-media:
-    // Convertir los operandos a flotante
-    scvtf d2, x2
-    scvtf d4, x3
-    // Dividimos el valor total por la cantidad de elementos
-    fdiv d0, d2, d4
-
-    // Mover el resultado (la suma) a x0 para devolverlo
-    fmov x0, d0
-    ret
+fin:
+    // Dirección de la cadena de formato
+    ldr x0, =fmt_float_add  
+    // Mueve el resultado a w1 (utilizando la parte simple precisión de d0)       
+    fmov w1, s0                    
+    ret   
