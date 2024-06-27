ca39b626c9693a5b4eb2d4544ac931b1a53a5f4e
JemimaSolmaira <2677413750719@ingenieria.usac.edu.gt>
2024-06-27 04:35:36 +0100

    fin

--- server/barometro/add_floats.s
+++ server/barometro/add_floats.s
@@ -1,18 +1,18 @@
 .global sum_array
 
 sum_array:
-// x0 = puntero al arreglo, x1 = n�mero de elementos
-    mov w2, #0        // Inicializar el �ndice
+// x0 = puntero al arreglo, x1 = n?mero de elementos
+    mov w2, #0        // Inicializar el ?ndice
     fmov s0, wzr      // Inicializar la suma a 0.0
 
 loop:
     ldr s1, [x0, x2, lsl #2]  // Cargar el siguiente valor del arreglo en s1
     fadd s0, s0, s1           // s0 += s1
-    add x2, x2, #1            // Incrementar el �ndice
-    cmp x2, x1                // Comparar el �ndice con el n�mero de elementos
-    b.lt loop                 // Si el �ndice es menor, repetir el bucle
+    add x2, x2, #1            // Incrementar el ?ndice
+    cmp x2, x1                // Comparar el ?ndice con el n?mero de elementos
+    b.lt loop                 // Si el ?ndice es menor, repetir el bucle
 
-    // Convertir x1 (n�mero de elementos) a flotante en s1
+    // Convertir x1 (n?mero de elementos) a flotante en s1
     ucvtf s1, x1              // s1 = (float)x1
     fdiv s0, s0, s1           // s0 /= s1
 
